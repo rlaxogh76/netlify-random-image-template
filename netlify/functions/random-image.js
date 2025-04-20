b8db3c58bd6fb1e63a4364b8011ea5320a160c84
rlaxogh76 <btm.email2769@gmail.com>
2025-04-20 12:21:17 +0900

    url changed

--- netlify/functions/random-image.js
+++ netlify/functions/random-image.js
@@ -1,11 +1,11 @@
 exports.handler = async function () {
   const images = [
-    "https://random-img-picker.netlify.app/images/blog-excited.gif",
-    "https://random-img-picker.netlify.app/images/blob-sign-no.gif",
-    "https://random-img-picker.netlify.app/images/blob-sign-yes.gif",
-    "https://random-img-picker.netlify.app/images/blob-snowball.gif",
-    "https://random-img-picker.netlify.app/images/blob-crazy-happy.gif",
-    "https://random-img-picker.netlify.app/images/blob-happy-peak.gif",
+    "https://peppy-melba-47ed98.netlify.app/images/blog-excited.gif",
+    "https://peppy-melba-47ed98.netlify.app/images/blob-sign-no.gif",
+    "https://peppy-melba-47ed98.netlify.app/images/blob-sign-yes.gif",
+    "https://peppy-melba-47ed98.netlify.app/images/blob-snowball.gif",
+    "https://peppy-melba-47ed98.netlify.app/images/blob-crazy-happy.gif",
+    "https://peppy-melba-47ed98.netlify.app/images/blob-happy-peak.gif",
   ];
 
   const random = images[Math.floor(Math.random() * images.length)];
