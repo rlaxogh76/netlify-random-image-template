5f7e682f862c4aae49cdb9e1aa4468e84a0731d0
rlaxogh76 <btm.email2769@gmail.com>
2025-07-31 22:45:42 +0900

    .

---
+++ netlify/functions/random-image.js
@@ -0,0 +1,30 @@
+exports.handler = async function () {
+  const images = [
+    "https://peppy-melba-47ed98.netlify.app/images/blog-excited.gif",
+    "https://peppy-melba-47ed98.netlify.app/images/blob-sign-no.gif",
+    "https://peppy-melba-47ed98.netlify.app/images/blob-sign-yes.gif",
+    "https://peppy-melba-47ed98.netlify.app/images/blob-crazy-happy.gif",
+    "https://peppy-melba-47ed98.netlify.app/images/blob-happy-peak.gif",
+    "https://peppy-melba-47ed98.netlify.app/images/ablobcaramelldansen.gif",
+    "https://peppy-melba-47ed98.netlify.app/images/ablobenjoy.gif",
+    "https://peppy-melba-47ed98.netlify.app/images/ablobenjoysnow.gif",
+    "https://peppy-melba-47ed98.netlify.app/images/aniblobpeak.gif",
+    "https://peppy-melba-47ed98.netlify.app/images/blobcat_code.gif",
+    "https://peppy-melba-47ed98.netlify.app/images/blobmusic2.gif",
+    "https://peppy-melba-47ed98.netlify.app/images/BlobZeroGravity.gif",
+    "https://peppy-melba-47ed98.netlify.app/images/comfyblobdrive.gif",
+    "https://peppy-melba-47ed98.netlify.app/images/Party_blod.gif",
+    "https://peppy-melba-47ed98.netlify.app/images/VoteBlob.gif",
+    "https://peppy-melba-47ed98.netlify.app/images/happycat.gif",
+    "https://peppy-melba-47ed98.netlify.app/images/pou-silly.gif",
+  ];
+
+  const random = images[Math.floor(Math.random() * images.length)];
+
+  return {
+    statusCode: 302,
+    headers: {
+      Location: random,
+    },
+  };
+};
